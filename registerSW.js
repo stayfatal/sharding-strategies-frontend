a5ba7a2ef0d6c13b7206122775c92cb1c3d4d7fb
s.khanifov <s.khanifov@ktsstudio.ru>
2026-05-27 00:24:19 +0300

    fix

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,7 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sharding-strategies-frontend/sw.js', {
+      scope: '/sharding-strategies-frontend/',
+    });
+  });
+}
